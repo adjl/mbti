144de1dcec48c4c41f61d01f0cfbfe2f3709d568
Adei Josol <adjl@users.noreply.github.com>
2012-01-08 22:37:45 +0000

    Made major refactoring - renamed variables and methods, made the class set_strengths method pure, created class set_preferences method and refactored set_personality_type method.

--- personality.py
+++ personality.py
@@ -13,79 +13,88 @@ Author: Helena 'Adei' Josol <helena.josol@gmail.com>
 
 class PersonalityType:
 	def __init__(self):
-		self.preferences = {}
-		self.preferences['E'], self.preferences['I'] = self.get_preference('EI')
-		self.preferences['S'], self.preferences['N'] = self.get_preference('SN')
-		self.preferences['T'], self.preferences['F'] = self.get_preference('TF')
-		self.preferences['J'], self.preferences['P'] = self.get_preference('JP')
-
-		self.personality_type = self.get_personality_type()
+		self.strengths = self.set_strengths()
+		self.preferences = self.set_preferences()
+		self.personality_type = self.set_personality_type()
 
 	def __str__(self, verbose=False):
 		output = self.personality_type
 
 		if verbose:
-			preference_names = {
+			strength_names = {
 				'E': 'Extraversion', 'I': 'Introversion',
 				'S': 'Sensing',      'N': 'iNtuition',
 				'T': 'Thinking',     'F': 'Feeling',
 				'J': 'Judging',      'P': 'Perceiving'
 			}
 
-			preference_list = 'EISNTFJP'
-			for preference in preference_list:
-				output += '{0:12} ({1}): {2:3d}\n'.format(preference_names[preference], preference, self.preferences[preference])
+			strength_list = 'EISNTFJP'
+			for strength in strength_list:
+				output += '{0:12} ({1}): {2:3d}\n'.format(strength_names[strength], strength, self.strengths[strength])
 			output = output.rstrip('\n')
 
 		return output
 
-	def get_preference(self, dichotomy):
+	def set_strengths(self):
 		import random
 
-		preferences = {
+		probabilities = {
 			'EI': 49.3,
 			'SN': 73.3,
 			'TF': 40.2,
 			'JP': 54.1
 		}
 
-		preference = round(random.uniform(0, 100), 1)
-		strength = random.randint(50, 100)
+		strengths = {}
 
-		if 0 <= preference <= preferences[dichotomy]:
-			return strength, 100 - strength
-		elif preferences[dichotomy] + 0.1 <= preference <= 100:
-			return 100 - strength, strength
+		for dichotomy in probabilities.keys():
+			probability = round(random.uniform(0, 100), 1)
+			strength = random.randint(50, 100)
 
-	def get_personality_type(self):
+			if probability <= probabilities[dichotomy]:
+				strengths[dichotomy[0]] = strength
+				strengths[dichotomy[1]] = 100 - strength
+			else:
+				strengths[dichotomy[0]] = 100 - strength
+				strengths[dichotomy[1]] = strength
+
+		return strengths
+
+	def set_preferences(self):
+		preferences = []
+
+		if self.strengths['E'] > self.strengths['I']:
+			preferences.append('E')
+		elif self.strengths['E'] < self.strengths['I']:
+			preferences.append('I')
+		else:
+			preferences.append('X')
+
+		if self.strengths['S'] > self.strengths['N']:
+			preferences.append('S')
+		elif self.strengths['S'] < self.strengths['N']:
+			preferences.append('N')
+		else:
+			preferences.append('X')
+
+		if self.strengths['T'] > self.strengths['F']:
+			preferences.append('T')
+		elif self.strengths['T'] < self.strengths['F']:
+			preferences.append('F')
+		else:
+			preferences.append('X')
+
+		if self.strengths['J'] > self.strengths['P']:
+			preferences.append('J')
+		elif self.strengths['J'] < self.strengths['P']:
+			preferences.append('P')
+		else:
+			preferences.append('X')
+
+		return preferences
+
+	def set_personality_type(self):
 		personality_type = ''
-
-		if self.preferences['E'] > self.preferences['I']:
-			personality_type += 'E'
-		elif self.preferences['E'] < self.preferences['I']:
-			personality_type += 'I'
-		else:
-			personality_type += 'X'
-
-		if self.preferences['S'] > self.preferences['N']:
-			personality_type += 'S'
-		elif self.preferences['S'] < self.preferences['N']:
-			personality_type += 'N'
-		else:
-			personality_type += 'X'
-
-		if self.preferences['T'] > self.preferences['F']:
-			personality_type += 'T'
-		elif self.preferences['T'] < self.preferences['F']:
-			personality_type += 'F'
-		else:
-			personality_type += 'X'
-
-		if self.preferences['J'] > self.preferences['P']:
-			personality_type += 'J'
-		elif self.preferences['J'] < self.preferences['P']:
-			personality_type += 'P'
-		else:
-			personality_type += 'X'
-
+		for preference in self.preferences:
+			personality_type += preference
 		return personality_type
