35f176634bb7cc06a142d1cd7398bb9ed8162f70
Adei Josol <adjl@users.noreply.github.com>
2012-01-08 20:17:15 +0000

    Added class get_personality_type function and modified class __str__ function. Fixed a bug in the class get_personality_type function.

--- personalityclasses.py
+++ personalityclasses.py
@@ -18,20 +18,21 @@ class PersonalityType:
 		self.preferences['S'], self.preferences['N'] = self.get_preference('SN')
 		self.preferences['T'], self.preferences['F'] = self.get_preference('TF')
 		self.preferences['J'], self.preferences['P'] = self.get_preference('JP')
+		self.personality_type = self.get_personality_type()
 
-	def __str__(self):
-		preference_list = 'EISNTFJP'
-		preference_names = {
-			'E': 'Extraversion', 'I': 'Introversion',
-			'S': 'Sensing',      'N': 'iNtuition',
-			'T': 'Thinking',     'F': 'Feeling',
-			'J': 'Judging',      'P': 'Perceiving'
-		}
-
-		output = ''
-		for preference in preference_list:
-			output += '{0:12} : {1:3d}\n'.format(preference_names[preference], self.preferences[preference])
-		output = output.rstrip('\n')
+	def __str__(self, verbose=False):
+		output = self.personality_type
+		if verbose:
+			preference_list = 'EISNTFJP'
+			preference_names = {
+				'E': 'Extraversion', 'I': 'Introversion',
+				'S': 'Sensing',      'N': 'iNtuition',
+				'T': 'Thinking',     'F': 'Feeling',
+				'J': 'Judging',      'P': 'Perceiving'
+			}
+			for preference in preference_list:
+				output += '{0:12} ({1}): {2:3d}\n'.format(preference_names[preference], preference, self.preferences[preference])
+			output = output.rstrip('\n')
 		return output
 
 	def get_preference(self, dichotomy):
@@ -50,3 +51,31 @@ class PersonalityType:
 			return strength, 100 - strength
 		elif preferences[dichotomy] + 0.1 <= preference <= 100:
 			return 100 - strength, strength
+
+	def get_personality_type(self):
+		personality_type = ''
+		if self.preferences['E'] > self.preferences['I']:
+			personality_type += 'E'
+		elif self.preferences['E'] < self.preferences['I']:
+			personality_type += 'I'
+		else:
+			personality_type += 'X'
+		if self.preferences['S'] > self.preferences['N']:
+			personality_type += 'S'
+		elif self.preferences['S'] < self.preferences['N']:
+			personality_type += 'N'
+		else:
+			personality_type += 'X'
+		if self.preferences['T'] > self.preferences['F']:
+			personality_type += 'T'
+		elif self.preferences['T'] < self.preferences['F']:
+			personality_type += 'F'
+		else:
+			personality_type += 'X'
+		if self.preferences['J'] > self.preferences['P']:
+			personality_type += 'J'
+		elif self.preferences['J'] < self.preferences['P']:
+			personality_type += 'P'
+		else:
+			personality_type += 'X'
+		return personality_type
