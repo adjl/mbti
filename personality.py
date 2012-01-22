9c4a862ae917e238506502d873dbd5af7e690d97
Adei Josol <adjl@users.noreply.github.com>
2012-01-22 19:51:35 +0000

    Created class variables and methods private

--- personality.py
+++ personality.py
@@ -13,14 +13,14 @@ Author: Helena 'Adei' Josol <helena.josol@gmail.com>
 
 class Personality:
 
-	temperament_names = {
+	_temperament_names = {
 		'SP': ['Artisan', 'Creator'],
 		'SJ': ['Guardian', 'Protector'],
 		'NF': ['Idealist', 'Visionary'],
 		'NT': ['Rational', 'Intellectual']
 	}
 
-	personality_type_names = {
+	_personality_type_names = {
 		'SP': {
 			'ESFP': ['Performer', 'Entertainer', 'Performer'],
 			'ESTP': ['Promoter', 'Persuader', 'Doer'],
@@ -47,7 +47,7 @@ class Personality:
 		}
 	}
 
-	preference_names = {
+	_preference_names = {
 		'E': ['Extraversion', 'Extravert', 'Extraverted'],
 		'I': ['Introversion', 'Introvert', 'Introverted'],
 		'S': ['Sensing', 'Sensor'],
@@ -58,7 +58,7 @@ class Personality:
 		'J': ['Judging', 'Judger']
 	}
 
-	cognitive_function_names = {
+	_cognitive_function_names = {
 		'Se': 'Extraverted Sensing',
 		'Si': 'Introverted Sensing',
 		'Ne': 'Extraverted iNtuition',
@@ -69,14 +69,14 @@ class Personality:
 		'Ti': 'Introverted Thinking'
 	}
 
-	temperament_probabilities = {
+	_temperament_probabilities = {
 		'SP': {'All': 0.33, 'Male': 0.34, 'Female': 0.32},
 		'SJ': {'All': 0.405, 'Male': 0.375, 'Female': 0.435},
 		'NF': {'All': 0.14, 'Male': 0.105, 'Female': 0.175},
 		'NT': {'All': 0.125, 'Male': 0.18, 'Female': 0.07}
 	}
 
-	personality_type_probabilities = {
+	_personality_type_probabilities = {
 		'SP': {
 			'ESFP': {'All': 0.11, 'Male': 0.08, 'Female': 0.14},
 			'ESTP': {'All': 0.1, 'Male': 0.125, 'Female': 0.075},
@@ -103,18 +103,25 @@ class Personality:
 		}
 	}
 
+	_preference = {
+		'Attitude': 0,
+		'Perceiving': 1,
+		'Judging': 2,
+		'Lifestyle': 3
+	}
 
-	def __init__(self):
-		self.temperament = self.set_temperament()
-		self.personality_type = self.set_personality_type(self.temperament)
-		self.cognitive_functions = self.set_cognitive_functions(self.personality_type)
+
+	def __init__(self, gender='All'):
+		self._temperament = self._set_temperament(gender)
+		self._personality_type = self._set_personality_type(self._temperament, gender)
+		self._cognitive_functions = self._set_cognitive_functions(self._personality_type)
 
 
 	def __str__(self):
-		return self.personality_type
+		return self._personality_type
 
 
-	def set_temperament(self):
+	def _set_temperament(self, gender):
 		import random
 
 		probability = 0
@@ -123,13 +130,13 @@ class Personality:
 		boundary = 0
 
 		# find which temperament range probability lies
-		for temperament in self.temperament_probabilities.keys():
-			if boundary < probability <= boundary + self.temperament_probabilities[temperament]:
+		for temperament in self._temperament_probabilities.keys():
+			if boundary < probability <= boundary + self._temperament_probabilities[temperament][gender]:
 				return temperament
-			boundary += self.temperament_probabilities[temperament]
+			boundary += self._temperament_probabilities[temperament][gender]
 
 
-	def set_personality_type(self, temperament):
+	def _set_personality_type(self, temperament, gender):
 		import random
 
 		probability = 0
@@ -138,73 +145,66 @@ class Personality:
 		boundary = 0
 
 		# find which personality type range probability lies
-		for personality_type in self.personality_type_probabilities[temperament].keys():
-			personality_type_probability = self.personality_type_probabilities[temperament][personality_type] / self.temperament_probabilities[temperament]
+		for personality_type in self._personality_type_probabilities[temperament].keys():
+			personality_type_probability = self._personality_type_probabilities[temperament][personality_type][gender] / self._temperament_probabilities[temperament][gender]
 			if boundary < probability <= boundary + personality_type_probability:
 				return personality_type
 			boundary += personality_type_probability
 
 
-	def set_cognitive_functions(self, personality_type):
-		preference = {
-			'Attitude': 0,
-			'Perceiving': 1,
-			'Judging': 2,
-			'Lifestyle': 3
-		}
-
+	def _set_cognitive_functions(self, personality_type):
 		personality_type = list(personality_type)
 		cognitive_functions = {}
 
-		if personality_type[preference['Attitude']] == 'E':
-			if personality_type[preference['Lifestyle']] == 'P':
-				if personality_type[preference['Perceiving']] == 'S':
+		if personality_type[self._preference['Attitude']] == 'E':
+			if personality_type[self._preference['Lifestyle']] == 'P':
+				if personality_type[self._preference['Perceiving']] == 'S':
 					cognitive_functions['Dominant'] = 'Se'
 					cognitive_functions['Inferior'] = 'Ni'
 				else:
 					cognitive_functions['Dominant'] = 'Ne'
 					cognitive_functions['Inferior'] = 'Si'
-				if personality_type[preference['Judging']] == 'F':
+				if personality_type[self._preference['Judging']] == 'F':
 					cognitive_functions['Auxiliary'] = 'Fi'
 					cognitive_functions['Tertiary'] = 'Te'
 				else:
 					cognitive_functions['Auxiliary'] = 'Ti'
 					cognitive_functions['Tertiary'] = 'Fe'
 			else:
-				if personality_type[preference['Judging']] == 'F':
+				if personality_type[self._preference['Judging']] == 'F':
 					cognitive_functions['Dominant'] = 'Fe'
 					cognitive_functions['Inferior'] = 'Ti'
 				else:
 					cognitive_functions['Dominant'] = 'Te'
 					cognitive_functions['Inferior'] = 'Fi'
-				if personality_type[preference['Perceiving']] == 'S':
+				if personality_type[self._preference['Perceiving']] == 'S':
 					cognitive_functions['Auxiliary'] = 'Si'
 					cognitive_functions['Tertiary'] = 'Ne'
 				else:
 					cognitive_functions['Auxiliary'] = 'Ni'
 					cognitive_functions['Tertiary'] = 'Se'
 		else:
-			if personality_type[preference['Lifestyle']] == 'P':
-				if personality_type[preference['Perceiving']] == 'S':
+			if personality_type[self._preference['Lifestyle']] == 'P':
+				if personality_type[self._preference['Perceiving']] == 'S':
 					cognitive_functions['Auxiliary'] = 'Se'
 					cognitive_functions['Tertiary'] = 'Ni'
 				else:
 					cognitive_functions['Auxiliary'] = 'Ne'
 					cognitive_functions['Tertiary'] = 'Si'
-				if personality_type[preference['Judging']] == 'F':
+				if personality_type[self._preference['Judging']] == 'F':
 					cognitive_functions['Dominant'] = 'Fi'
 					cognitive_functions['Inferior'] = 'Te'
 				else:
 					cognitive_functions['Dominant'] = 'Ti'
 					cognitive_functions['Inferior'] = 'Fe'
 			else:
-				if personality_type[preference['Judging']] == 'F':
+				if personality_type[self._preference['Judging']] == 'F':
 					cognitive_functions['Auxiliary'] = 'Fe'
 					cognitive_functions['Tertiary'] = 'Ti'
 				else:
 					cognitive_functions['Auxiliary'] = 'Te'
 					cognitive_functions['Tertiary'] = 'Fi'
-				if personality_type[preference['Perceiving']] == 'S':
+				if personality_type[self._preference['Perceiving']] == 'S':
 					cognitive_functions['Dominant'] = 'Si'
 					cognitive_functions['Inferior'] = 'Ne'
 				else:
