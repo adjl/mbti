dddbfa122709c84bfaa19e10ac8606a85b207efd
Adei Josol <adjl@users.noreply.github.com>
2012-01-24 22:08:59 +0000

    Added a file store that I *might* need

---
+++ code_snippets.py
@@ -0,0 +1,55 @@
+_temperament_names = {
+	'SP': ['Artisan', 'Creator'],
+	'SJ': ['Guardian', 'Protector'],
+	'NF': ['Idealist', 'Visionary'],
+	'NT': ['Rational', 'Intellectual']
+}
+
+_personality_type_names = {
+	'SP': {
+		'ESFP': ['Performer', 'Entertainer', 'Performer'],
+		'ESTP': ['Promoter', 'Persuader', 'Doer'],
+		'ISFP': ['Composer', 'Artist', 'Artist'],
+		'ISTP': ['Crafter', 'Craftsman', 'Mechanic']
+	},
+	'SJ': {
+		'ESFJ': ['Provider', 'Supporter', 'Caregiver'],
+		'ESTJ': ['Supervisor', 'Overseer', 'Guardian'],
+		'ISFJ': ['Protector', 'Defender', 'Nurturer'],
+		'ISTJ': ['Inspector', 'Examiner', 'Duty Fulfiller']
+	},
+	'NF': {
+		'ENFP': ['Champion', 'Advocate', 'Inspirer'],
+		'ENFJ': ['Teacher', 'Mentor', 'Giver'],
+		'INFP': ['Healer', 'Dreamer', 'Idealist'],
+		'INFJ': ['Counselor', 'Confidant', 'Protector']
+	},
+	'NT': {
+		'ENTP': ['Inventor', 'Originator', 'Visionary'],
+		'ENTJ': ['Fieldmarshal', 'Chief', 'Executive'],
+		'INTP': ['Architect', 'Engineer', 'Thinker'],
+		'INTJ': ['Mastermind', 'Strategist', 'Scientist']
+	}
+}
+
+_preference_names = {
+	'E': ['Extraversion', 'Extravert', 'Extraverted'],
+	'I': ['Introversion', 'Introvert', 'Introverted'],
+	'S': ['Sensing', 'Sensor'],
+	'N': ['iNtuition', 'iNtuitor', 'iNtuitive'],
+	'F': ['Feeling', 'Feeler'],
+	'T': ['Thinking', 'Thinker'],
+	'P': ['Perceiving', 'Perceiver'],
+	'J': ['Judging', 'Judger']
+}
+
+_cognitive_function_names = {
+	'Se': 'Extraverted Sensing',
+	'Si': 'Introverted Sensing',
+	'Ne': 'Extraverted iNtuition',
+	'Ni': 'Introverted iNtuition',
+	'Fe': 'Extraverted Feeling',
+	'Fi': 'Introverted Feeling',
+	'Te': 'Extraverted Thinking',
+	'Ti': 'Introverted Thinking'
+}
